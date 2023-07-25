8afd5b0291eb5c19d80ac8d252e9fe7ebe095ed2
Roberto <roberto.quadraccia90@gmail.com>
2023-07-25 11:26:13 +0100

    Class second test done

--- lib/present.py
+++ lib/present.py
@@ -12,3 +12,11 @@ class Present():
             raise Exception('No contents have been wrapped')
         return self.contents
     
+    # We can't test both because they would conflict with each other,
+    # therefore if we test the wrap(), if self.content = None it will fail,
+    # because the EXCEPTION is ONLY raised if self.content = None
+
+    # Note: if we change self.content = 90, hence the EXCEPTION is met,
+    # (in this case the condition is self.content IS NOT None),
+    # it means that the test will PASS, otherwise the test will FAIL.
+    
--- tests/test_present.py
+++ tests/test_present.py
@@ -1,25 +1,42 @@
 import pytest
 from lib.present import *
 
+"""
+When we wrap an item
+We get it back unwrapping
+"""
 
-
-
-def test_present_2():
+def test_wrap_and_then_unwrap():
     pres = Present()
-    # pres.wrap(10)
-    # assert pres.unwrap() == 10
+    pres.wrap(33)
+    assert pres.unwrap() == 33
 
+"""
+If we unwrap before wrapping
+We get an error message
+"""
+
+def test_unwrap_without_wrapping():
+    pres = Present()
     with pytest.raises(Exception) as e:
-        pres.wrap('1')
+        pres.unwrap()
+    error_msg = str(e.value)
+    assert error_msg == 'No contents have been wrapped'
+
+"""
+If we try to wrap an alread-wrapped present
+We get an error message
+"""
+
+def test_wrapping_already_wrap():
+    pres = Present()
+    pres.wrap(44) 
+    with pytest.raises(Exception) as e:
+        pres.wrap(66)
     error_message = str(e.value)
     assert error_message == "A contents has already been wrapped."
 
-# def test_present():
-#     pres = Present()
 
-#     with pytest.raises(Exception) as x:
-#         pres.unwrap()
-#     error_msg = str(x.value)
-#     assert error_msg == 'No contents have been wrapped'
+
 
     
